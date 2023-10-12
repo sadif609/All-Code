3f0e68c06511fed056a5b476511a83c54352755d
sadif609 <sadif609@gmail.com>
2023-10-12 22:35:57 +0600

    cp

---
+++ Competitive programming/week2/ashiq.cpp
@@ -0,0 +1,37 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n,k=0;
+        cin>>n;
+        char s[n+2][n+2];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+                cin>>s[i][j];
+        }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0,y=n-1; j<n/2; j++,y--)
+            {
+                if(s[i][j]!=s[i][y])
+                    k++;
+            }
+             for(int j=0,y=n-1; j<n/2; j++,y--)
+            {
+                if(s[j][i]!=s[y][i])
+                    k++;
+            }
+
+
+        }
+        cout<<k<<endl;
+    }
+}
